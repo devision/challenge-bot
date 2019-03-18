9aaa8dd6db040c89890990c7cdc08b6adebb0f54
devision <2677401+devision@users.noreply.github.com>
2019-03-18 19:42:44 +0000

    Implement username registration rejection flow, score saving.

--- challenge.js
+++ challenge.js
@@ -11,6 +11,56 @@ const AUTH_HEADER = {Authorization: `Bearer ${AUTH_TOKEN}`};
 
 let api = {};
 let contest_options;
+let lastUpdate = Date.now();
+
+async function updateScore(telegram_id, score, title){
+  let sqliteInit;
+  let query;
+  let scores;
+  let localId = {};
+
+  try {
+    sqliteInit = await sqlitePromise;
+  }
+  catch (error){
+    return -2;
+  }
+  try {
+    query = await sqliteInit.all('SELECT scores FROM bot WHERE telegram_id = ?;',[telegram_id]);
+    scores = JSON.parse(query[0].scores);
+    scores[title] = score;
+    await sqliteInit.all('UPDATE bot SET scores = ? WHERE telegram_id = ?;'
+      ,[JSON.stringify(scores), telegram_id]);    
+  }
+  catch (error){
+    return -1;
+  }
+
+  return score;
+}
+
+async function currentScores(title){
+  let sqliteInit;
+  let query;
+  try {
+    sqliteInit = await sqlitePromise;
+  }
+  catch (error){
+    return 2;
+  }
+  try {
+    query = await sqliteInit.all('SELECT network_name, scores FROM bot;');
+  }
+  catch (error){
+    return 1;
+  }
+  console.log(query[0].network_name);
+  console.log(JSON.parse(query[0].network_name).IIDX);
+  console.log(JSON.parse(query[0].scores).IIDX);
+  console.log(JSON.parse(query[1].network_name).IIDX);
+  console.log(JSON.parse(query[1].scores).IIDX);
+  return query[0];
+}
 
 async function retrieveIdLocal(telegram_id, title){
   let sqliteInit;
@@ -20,19 +70,15 @@ async function retrieveIdLocal(telegram_id, title){
   try {
     sqliteInit = await sqlitePromise;
   }
-
   catch (error){
     return 2;
   }
-
   try {
     query = await sqliteInit.all('SELECT network_id FROM bot WHERE telegram_id = ?;',[telegram_id]);
   }
-
   catch (error){
     return 1;
   }
-
   return JSON.parse(query[0].network_id);
 }
 
@@ -44,11 +90,9 @@ async function registerTitle(telegram_id, title){
   try {
     sqliteInit = await sqlitePromise;
   }
-
   catch (error){
     return 2;
   }
-
   try {
     query = await sqliteInit.all('SELECT registered_titles FROM bot WHERE telegram_id = ?;',[telegram_id]);
     titles = JSON.parse(query[0].registered_titles);
@@ -56,11 +100,9 @@ async function registerTitle(telegram_id, title){
     await sqliteInit.all('UPDATE bot SET registered_titles = ? WHERE telegram_id = ?;'
       ,[JSON.stringify(titles), telegram_id]);
   }
-
   catch (error){
     return 1;
   }
-
   return 0;
 }
 
@@ -72,24 +114,23 @@ async function updateNetworkName(telegram_id, networkName, title){
   try {
     sqliteInit = await sqlitePromise;
   }
-
   catch (error){
     throw error;
   }
-
   try {
     query = await sqliteInit.all('SELECT network_name FROM bot WHERE telegram_id = ?;',[telegram_id]);
+    console.log('Names - updateNetworkName');
+    console.log(names);
     names = JSON.parse(query[0].network_name);
     names[title] = networkName;
+    console.log(names);
     await sqliteInit.all('UPDATE bot SET network_name = ? WHERE telegram_id = ?;'
       ,[JSON.stringify(names), telegram_id]);
   }
-
   catch (error){
+    console.log('error');
     return 1;
   }
-
-
 }
 
 async function updateId(telegram_id, networkId, title){
@@ -100,11 +141,9 @@ async function updateId(telegram_id, networkId, title){
   try {
     sqliteInit = await sqlitePromise;
   }
-
   catch (error){
     throw error;
   }
-
   try {
     query = await sqliteInit.all('SELECT network_id FROM bot WHERE telegram_id = ?;',[telegram_id]);
     console.log(query);
@@ -115,11 +154,9 @@ async function updateId(telegram_id, networkId, title){
       ,[JSON.stringify(ids), telegram_id]);
     console.log(ids);
   }
-
   catch (error){
     return 1;
   }
-
 }
 
 async function getContestData(){
@@ -130,19 +167,14 @@ async function getContestData(){
   try {
     sqliteInit = await sqlitePromise;
   }
-
   catch (error){
     throw error;
   }
-
   let dbResult = await sqliteInit.all('SELECT * FROM contest;');
-
   dbResult.forEach((current, index) => {
     contestData[current.title] = {version: current.version, chart_id: current.chart_id}; 
   });
-
   console.log(contestData);
-
   return contestData;
 }
 
@@ -156,42 +188,36 @@ async function getCurrentState(telegram_id){
   catch (error){
     throw error;
   }
-
   try {
     currentState = await sqliteInit.all('SELECT current_state, last_command FROM bot WHERE telegram_id = ?;', [telegram_id]);
   }
-
   catch (error){
     throw error;
   }
-
   return currentState[0];
 }
 
 async function updateState(telegram_id, currentState, lastCommand){
   let sqliteInit;
+  let test;
+
   try {
     sqliteInit = await sqlitePromise;
   }
   catch (error){
-    throw error;
+    return 2;
   }
-
-  let test;
-
   try {
     test = await sqliteInit.all('UPDATE bot SET current_state = ?, last_command = ? WHERE telegram_id = ?;'
-    ,[currentState,lastCommand,telegram_id]);  
+    ,[currentState,lastCommand,telegram_id]); 
+    return 0; 
   }
-
   catch (error){
-    throw error;
+    return 2;
   }
-
-  console.log(test);
 }
 
-async function isRegistered(telegram_id){
+async function checkRegistry(telegram_id, title){
   let sqliteInit;
   try {
     sqliteInit = await sqlitePromise;
@@ -199,10 +225,8 @@ async function isRegistered(telegram_id){
   catch (error){
     throw error;
   }
-
-  let check = await sqliteInit.all('SELECT * FROM bot WHERE telegram_id = ?',[telegram_id]);
-  if (check.length === 0) return false;
-  else if (check[0].telegram_id === telegram_id.toString()) return true;
+  let check = await sqliteInit.all('SELECT registered_titles FROM bot WHERE telegram_id = ?',[telegram_id]);
+  if((JSON.parse(check[0].registered_titles)[title]) === '1') return true;
   return false;
 }
 
@@ -242,7 +266,6 @@ async function initializeDatabase() {
   try{
     sqliteInit = await sqlitePromise;
   }
-
   catch (error){
     throw error;
   }
@@ -271,8 +294,8 @@ async function initializeDatabase() {
 
         await sqliteInit.all('INSERT INTO `contest`(`title`,`version`,`chart_id`) VALUES (?,?,?), (?,?,?);',
           ['iidx',24,'HjfscNcHDQt','sdvx',4,'C3qHc_ayquL']);
-
       }
+
       sqliteConfirmColumns = await sqliteInit.all("PRAGMA TABLE_INFO('bot');");
       if (sqliteConfirmColumns[0].name === 'telegram_id'
         && sqliteConfirmColumns[1].name === 'is_registered'
@@ -285,9 +308,7 @@ async function initializeDatabase() {
         return ('DB_INIT_SUCCESS');
       }
   }
-
   return new Error('DB_INIT_FAILURE');
-
 }
 
 async function initApi(){
@@ -375,10 +396,13 @@ async function getScore(options, url, currentScore = 0){
   return score;
 }
 
-async function getId(options) {
+async function getId(options, title) {
   let request;
+  let url;
+  if (title === 'IIDX') url = api.iidx[options.version]._links.profiles + `?dj_name=${options.player_name}`;
+  else if (title === 'SDVX') url = api.sdvx[options.version]._links.profiles + `?name=${options.player_name}`;
   const requestParameters = {
-    url: api.iidx[options.version]._links.profiles + `?dj_name=${options.player_name}`,
+    url: url,
     json: true,
     headers: AUTH_HEADER,
     gzip: true
@@ -387,18 +411,17 @@ async function getId(options) {
     request = await rp(requestParameters);
   }
   catch (error) {
-    console.log(error);
     throw error;
   }
   if (request._items[0] === undefined) throw (new Error('NO_PLAYER_DATA'));
   else return (request._items[0]._id);
 }
 
-async function getPlayerData(options) {
+async function getPlayerData(options, title) {
   let id;
 
   try {
-    id = await getId(options);
+    id = await getId(options, title);
   }
 
   catch(error){
@@ -406,52 +429,65 @@ async function getPlayerData(options) {
   }
 
   let request;
-  const requestParameters = {
-    url: api.iidx[options.version]._links.profiles + `?_id=${id}`,
-    json: true,
-    headers: AUTH_HEADER,
-    gzip: true
-  };
+  let requestParameters = {};
+  let playerData = {};
 
-  try {
-    request = await rp(requestParameters);
-  }
-  catch (error) {
-    throw error;
-  }
-  const playerData = {
-    dj_name: request._items[0].dj_name,
-    iidx_id: request._items[0].iidx_id,
-    sp_rank: request._items[0].sp.rank,
-    dp_rank: request._items[0].dp.rank,
-    sp_points: request._items[0].sp.dj_points,
-    dp_points: request._items[0].dp.dj_points,
-    network_id: id
-  };
+  if (title === 'IIDX'){
+    requestParameters = {
+      url: api.iidx[options.version]._links.profiles + `?_id=${id}`,
+      json: true,
+      headers: AUTH_HEADER,
+      gzip: true
+    };
+    try {
+      request = await rp(requestParameters);
+    }
+    catch (error) {
+      throw error;
+    }
+    playerData = {
+      dj_name: request._items[0].dj_name,
+      iidx_id: request._items[0].iidx_id,
+      sp_rank: request._items[0].sp.rank,
+      dp_rank: request._items[0].dp.rank,
+      sp_points: request._items[0].sp.dj_points,
+      dp_points: request._items[0].dp.dj_points,
+      network_id: id
+    };
 
-  for (let key in playerData) {
-    if (playerData[key] === null) playerData[key]='No data';
+    for (let key in playerData) {
+      if (playerData[key] === null) playerData[key]='No data';
+    }
   }
+
+  if (title === 'SDVX'){
+    requestParameters = {
+      url: api.sdvx[options.version]._links.profiles + `?_id=${id}`,
+      json: true,
+      headers: AUTH_HEADER,
+      gzip: true
+    };
+    try {
+      request = await rp(requestParameters);
+    }
+    catch (error) {
+      throw error;
+    }
+    playerData = {
+      name: request._items[0].name,
+      sdvx_id: request._items[0].sdvx_id,
+      network_id: id
+    };
+
+    for (let key in playerData) {
+      if (playerData[key] === null) playerData[key]='No data';
+    }
+  }
+
   return playerData;
 }
 
-async function playerData(options) {
-  try {
-    options.url = api.iidx[options.version]._links.player_bests;
-    const id = await getId(options);
-    options.profile_id = id;
-    options.url = options.url + `?profile_id=${id}`;
-    const score = await getScore(options, options.url);
-    const sqliteInit = await sqlitePromise;
-    sqlitePushScore = await sqliteInit.all('INSERT INTO scores (userid, username, score) VALUES(?, ?, ?)',
-      [options.profile_id, options.player_name, score]);
-  }
 
-  catch (error) {
-    throw error;
-  }
-}
-
-module.exports = {getId, getScore, initApi, playerData, getContestData, initializeApplication, initializeDatabase, 
-                  sqlitePromise, getPlayerData, initUser, isRegistered, updateState, getCurrentState, registerTitle, 
-                  updateNetworkName, updateId, retrieveIdLocal, api};
+module.exports = {getId, getScore, initApi, getContestData, initializeApplication, initializeDatabase, 
+                  sqlitePromise, getPlayerData, initUser, checkRegistry, updateState, getCurrentState, registerTitle, 
+                  updateNetworkName, updateId, retrieveIdLocal, api, updateScore,currentScores, lastUpdate};
--- index.js
+++ index.js
@@ -33,6 +33,9 @@ challenge.initializeApplication().then((success) => {
 
 .then((result) => {
   contestData = result;
+  challenge.currentScores();
+  console.log(new Date(challenge.lastUpdate).toString());
+  console.log(challenge.api.sdvx[4]._links.profiles);
 })
 
 .then(() => {
@@ -57,82 +60,37 @@ challenge.initializeApplication().then((success) => {
   });
 
   bot.onText(/\/test/i, (msg, match) => {
-    challenge.retrieveIdLocal(msg.from.id,'IIDX').then((id) => {
-      let options = {
-        player_name: msg.text.toUpperCase(),
-        version: contestData.iidx.version,
-        chart_id: contestData.iidx.chart_id
-      };
-
-      let url = `${challenge.api.iidx[options.version]._links.player_bests}?profile_id=${id.IIDX}`;
-      return challenge.getScore(options, url);
-    })
-
-    .then((score) => {
-      bot.sendMessage(msg.chat.id, `${score}`);
-    });
+      bot.sendMessage(msg.from.id,
+        `*Current standings for Sound Voltex*\n\n`+
+        `*1st* - Player1 - 1874\n`+
+        `*2nd* - Player2 - 1594\n`+
+        `*3rd* - Player3 - 1567\n`+
+        `*4th* - Player4 - 1478\n`+
+        `*5th* - Player5 - 1426\n`
+        ,{parse_mode:'Markdown'});
   });
 
-  bot.onText(/\/register ([^\s]+)/i, (msg, match) => {
-    console.log(contestData);
-    if (msg.chat.type != "private"){
-      bot.sendMessage(msg.chat.id, 'Please message bot privately to register.');
-    }
-    else {
-      let options = {
-        player_name: msg.text.toUpperCase(),
-        version: contestData.iidx.version,
-        chart_id: contestData.iidx.chart_id
-      };
-
-      challenge.initUser(msg.from.id).then((result) => {
-        if (result){
-          challenge.getPlayerData(options).then((response) => {
-            if (response === 1) bot.sendMessage(msg.chat.id, 'Player wasn\'t found, please try again.');
-            else bot.sendMessage(msg.chat.id,
-              `Hello, ${msg.from.username}.\n`+
-              `Does this look like you?\n`+
-              `*DJ Name:*${response.dj_name}\n`+
-              `*IIDX ID:*${response.iidx_id}\n`+
-              `*SP Rank:*${response.sp_rank}\n`+
-              `*DP Rank:*${response.dp_rank}\n`+
-              `*SP Points:*${response.sp_points}\n`+
-              `*DP Points:*${response.dp_points}\n`
-              ,{parse_mode:'Markdown',
-                reply_markup:{inline_keyboard:[[{text:'Yes', callback_data:'REG_CONFIRM'},{text:'No', callback_data:'REG_DENY'}]]}
-            });
-              console.log(msg);
-          });
-        }
-
-        else if (result === 1) {
-          bot.sendMessage(msg.chat.id, 'You have already registered');
-        }
-
-        else if (result > 1) {
-          bot.sendMessage(msg.chat.id, 'Something bad has happened. Please message bot owner and let them know. Thanks!');
-        }
-
-      });
-    }
-});
-
   bot.onText(/\/register/i, (msg, match) => {
-    challenge.updateState(msg.from.id, 'REG_INIT', 'REGISTER');
     if (msg.chat.type != "private"){
       bot.sendMessage(msg.chat.id, 'Please message bot privately to register');
     }
     else{
-      bot.sendMessage(msg.chat.id,
-        'Please select a challenge to participate in.',
-        {reply_markup:{
-          inline_keyboard:
-            [[{text:'Beatmania',callback_data:'REG_REQ_IIDX'},
-              {text:'Sound Voltex', callback_data:'REG_REQ_SDVX'}
-            ]]
-          }
+      challenge.updateState(msg.from.id, 'REG_INIT', 'REGISTER').then((result) => {
+        console.log(result);
+        if (result === 0) {
+          bot.sendMessage(msg.chat.id,
+            'Please select a challenge to participate in.',
+              {reply_markup:{
+                inline_keyboard:
+                  [[{text:'Beatmania',callback_data:'REG_REQ_IIDX'},
+                    {text:'Sound Voltex', callback_data:'REG_REQ_SDVX'}
+                  ]]
+                }
+              }
+          )
         }
-      )
+        else bot.sendMessage(msg.chat.id,'Something went wrong, please message bot owner.');
+      })
     }
   });
 
@@ -150,35 +108,62 @@ challenge.initializeApplication().then((success) => {
 
   bot.onText(/^[^/]{1}[^\s]*$/i, (msg, match) => {
     challenge.getCurrentState(msg.from.id).then((state) => {
-      if (state.current_state === 'REG_NAME_AWAIT_IIDX') {
-        let options = {
-          player_name: msg.text.toUpperCase(),
-          version: contestData.iidx.version,
-          chart_id: contestData.iidx.chart_id
-        };
-        challenge.getPlayerData(options).then((response) => {
-          console.log(response);
-          if (response === 1) bot.sendMessage(msg.chat.id, 'Player wasn\'t found, please try again.');
-          else {
-            challenge.updateState(msg.from.id, 'REG_CONFIRM_AWAIT_IIDX', 'REG_NAME_ENTRY')
-            .then(() => challenge.updateNetworkName(msg.from.id, response.dj_name, 'IIDX'))
-            .then(() => challenge.updateId(msg.from.id, response.network_id, 'IIDX'))
-            .then(() => {
-              bot.sendMessage(msg.chat.id,
-                `Does this look like you?\n`+
-                `*DJ Name:*${response.dj_name}\n`+
-                `*IIDX ID:*${response.iidx_id}\n`+
-                `*SP Rank:*${response.sp_rank}\n`+
-                `*DP Rank:*${response.dp_rank}\n`+
-                `*SP Points:*${response.sp_points}\n`+
-                `*DP Points:*${response.dp_points}\n`
-              ,{parse_mode:'Markdown',
-                reply_markup:{inline_keyboard:[[{text:'Yes', callback_data:'REG_CONFIRM_IIDX'},{text:'No', callback_data:'REG_DENY_IIDX'}]]}
+      let options = {};
+      switch (state.current_state){
+        case 'REG_NAME_AWAIT_IIDX':
+          options = {
+            player_name: msg.text.toUpperCase(),
+            version: contestData.iidx.version,
+            chart_id: contestData.iidx.chart_id
+          };
+          challenge.getPlayerData(options, 'IIDX').then((response) => {
+            console.log(response);
+            if (response === 1) bot.sendMessage(msg.chat.id, 'Player wasn\'t found, please try again.');
+            else {
+              challenge.updateState(msg.from.id, 'REG_CONFIRM_AWAIT_IIDX', 'REG_NAME_ENTRY')
+              .then(() => challenge.updateNetworkName(msg.from.id, response.dj_name, 'IIDX'))
+              .then(() => challenge.updateId(msg.from.id, response.network_id, 'IIDX'))
+              .then(() => {
+                bot.sendMessage(msg.chat.id,
+                  `Does this look like you?\n`+
+                  `*DJ Name:*${response.dj_name}\n`+
+                  `*IIDX ID:*${response.iidx_id}\n`+
+                  `*SP Rank:*${response.sp_rank}\n`+
+                  `*DP Rank:*${response.dp_rank}\n`+
+                  `*SP Points:*${response.sp_points}\n`+
+                  `*DP Points:*${response.dp_points}\n`
+                ,{parse_mode:'Markdown',
+                  reply_markup:{inline_keyboard:[[{text:'Yes', callback_data:'REG_CONFIRM_IIDX'},{text:'No', callback_data:'REG_DENY_IIDX'}]]}
+                });
               });
-            });
-          }
-        });
-        challenge.getPlayerData(options)
+            }
+          });
+          break;
+
+        case 'REG_NAME_AWAIT_SDVX':
+          options = {
+            player_name: msg.text.toUpperCase(),
+            version: contestData.sdvx.version,
+            chart_id: contestData.sdvx.chart_id
+          };
+          challenge.getPlayerData(options,'SDVX').then((response) => {
+            if (response === 1) bot.sendMessage(msg.chat.id, 'Player wasn\'t found, please try again.');
+            else {
+              challenge.updateState(msg.from.id, 'REG_CONFIRM_AWAIT_SDVX', 'REG_NAME_ENTRY')
+              .then(() => challenge.updateNetworkName(msg.from.id, response.name, 'SDVX'))
+              .then(() => challenge.updateId(msg.from.id, response.network_id, 'SDVX'))
+              .then(() => {
+                bot.sendMessage(msg.chat.id,
+                  `Does this look like you?\n`+
+                  `*PLayer name:*${response.name}\n`+
+                  `*SDVX ID:*${response.sdvx_id}\n`
+                ,{parse_mode:'Markdown',
+                  reply_markup:{inline_keyboard:[[{text:'Yes', callback_data:'REG_CONFIRM_SDVX'},{text:'No', callback_data:'REG_DENY_SDVX'}]]}
+                });
+              });
+            }
+          });
+          break;
       }
     })
   });
@@ -186,24 +171,53 @@ challenge.initializeApplication().then((success) => {
   bot.on('callback_query', (callback) => {
     switch (callback.data) {
       case 'REG_REQ_IIDX':
-        challenge.updateState(callback.from.id, 'REG_NAME_AWAIT_IIDX', 'REG_NAME_CALLBACK_IIDX')
-        bot.answerCallbackQuery(callback.id);
-        bot.editMessageText('Send me your IIDX DJ name, so I can remember it.',
-          {
-            "chat_id":callback.message.chat.id,
-            "message_id":callback.message.message_id
+        challenge.checkRegistry(callback.from.id, 'IIDX').then((result) => {
+          if (result === true){
+            bot.answerCallbackQuery(callback.id);
+            bot.editMessageText('You have already been registered for this title.',
+              {
+                "chat_id":callback.message.chat.id,
+                "message_id":callback.message.message_id
+              }
+            );
           }
-        );
-        break;
+          else {
+            challenge.updateState(callback.from.id, 'REG_NAME_AWAIT_IIDX', 'REG_NAME_CALLBACK_IIDX')
+            bot.answerCallbackQuery(callback.id);
+            bot.editMessageText('Alright, send me your IIDX DJ name.',
+              {
+                "chat_id":callback.message.chat.id,
+                "message_id":callback.message.message_id
+              }
+            );         
+          }
+
+        });
+      break; 
       case 'REG_REQ_SDVX':
-        bot.answerCallbackQuery(callback.id);
-        bot.editMessageText('Send me your SDVX account name, so I can remember it.',
-          {
-            "chat_id":callback.message.chat.id,
-            "message_id":callback.message.message_id
+        challenge.checkRegistry(callback.from.id, 'SDVX').then((result) => {
+          if (result === true){
+            bot.answerCallbackQuery(callback.id);
+            bot.editMessageText('You have already been registered for this title.',
+              {
+                "chat_id":callback.message.chat.id,
+                "message_id":callback.message.message_id
+              }
+            );
           }
-        );
-        break;
+          else {
+            challenge.updateState(callback.from.id, 'REG_NAME_AWAIT_SDVX', 'REG_NAME_CALLBACK_SDVX')
+            bot.answerCallbackQuery(callback.id);
+            bot.editMessageText('Alright, send me your SDVX profile name.',
+              {
+                "chat_id":callback.message.chat.id,
+                "message_id":callback.message.message_id
+              }
+            );         
+          }
+
+        });
+      break;;
       case 'REG_CONFIRM_IIDX':
         challenge.registerTitle(callback.from.id, "IIDX");
         challenge.updateState(callback.from.id, 'REG_COMPLETE_IIDX', 'REG_CALLBACK_CONFIRM');
@@ -224,12 +238,72 @@ challenge.initializeApplication().then((success) => {
           })
 
         .then((score) => {
+          return challenge.updateScore(callback.from.id, score, 'IIDX');})
+        .then((score) => {
           bot.answerCallbackQuery(callback.id);
           if(score === 0) bot.sendMessage(callback.from.id, 
-              `Looks like you haven't played this chart yet.\n`+
-              `Don't worry, your score will appear in the contest once you play it.`);
+            `Looks like you haven't played this chart yet.\n`+
+            `Don't worry, your score will appear in the contest once you play it.`);
           else bot.sendMessage(callback.from.id, `Found it! Your high score for the current chart is ${score}.`);
-        });
+        })
+        break;
+
+      case 'REG_CONFIRM_SDVX':
+        challenge.registerTitle(callback.from.id, "SDVX");
+        challenge.updateState(callback.from.id, 'REG_COMPLETE_SDVX', 'REG_CALLBACK_CONFIRM');
+        bot.editMessageText('Awesome, your name has been registered!\n\nI am looking for your score now, which may take a couple of seconds.',
+          {
+            "chat_id":callback.message.chat.id,
+            "message_id":callback.message.message_id
+          }
+        );
+        challenge.retrieveIdLocal(callback.from.id,'SDVX').then((id) => {
+          let options = {
+            version: contestData.sdvx.version,
+            chart_id: contestData.sdvx.chart_id
+          };
+
+          let url = `${challenge.api.sdvx[options.version]._links.player_bests}?profile_id=${id.SDVX}`;
+          return challenge.getScore(options, url);
+          })
+          .then((score) => {
+            return challenge.updateScore(callback.from.id, score, 'SDVX');
+          })
+          .then((score) => {
+            if(score === 0) {
+              bot.sendMessage(callback.from.id, 
+                `Looks like you haven't played this chart yet.\n`+
+                `Don't worry, your score will appear in the contest once you play it.`);
+              bot.answerCallbackQuery(callback.id);
+            }
+            else {
+              bot.sendMessage(callback.from.id, `Found it! Your high score for the current chart is ${score}.`);
+              bot.answerCallbackQuery(callback.id);
+            }
+          });
+          break;
+
+      case 'REG_DENY_IIDX':
+        challenge.updateState(callback.from.id, 'REG_NAME_AWAIT_IIDX', 'REG_NAME_CALLBACK_IIDX')
+        bot.answerCallbackQuery(callback.id);
+        bot.editMessageText(`No problem, let's try again!\nSend me your IIDX DJ name.`,
+          {
+            "chat_id":callback.message.chat.id,
+            "message_id":callback.message.message_id
+          }
+        );
+        break;
+
+      case 'REG_DENY_SDVX':
+        challenge.updateState(callback.from.id, 'REG_NAME_AWAIT_SDVX', 'REG_NAME_CALLBACK_SDVX')
+        bot.answerCallbackQuery(callback.id);
+        bot.editMessageText(`No problem, let's try again!\nSend me your SDVX player name.`,
+          {
+            "chat_id":callback.message.chat.id,
+            "message_id":callback.message.message_id
+          }
+        );
+        break; 
 
     }
   });
